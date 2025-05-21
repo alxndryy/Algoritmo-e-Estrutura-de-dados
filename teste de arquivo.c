1ee9263dbef76d93d4d019fea2553d9e91408aa1
Alexandre Pereira <alexandredesouzapereirasamon@gmail.com>
2025-05-20 23:22:21 -0300

    Update teste de arquivo.c

--- teste de arquivo.c
+++ teste de arquivo.c
@@ -2,15 +2,17 @@
 #include<stdlib.h>
 
 int main (){
-char str[100];
-FILE* fp;
-fp= fopen("arquivo.txt","r");
-if (fp==NULL){
-	printf("Erro ao abrir o arquivo.\n");
-	return 1;
+	char str[100];
+	FILE* fp;
+
+	fp= fopen("arquivo.txt","r");
+	if (fp==NULL){
+		printf("Erro ao abrir o arquivo.\n");
+		return 1;
+	}
+	fscanf(fp,"%s",str);
+	printf("%s",str);
+	fclose(fp);
+	return 0;
 }
-fscanf(fp,"%s",str);
-printf("%s",str);
-fclose(fp);
-return 0;
-}
+//Corrigi esse espaçamento pq tava foda pqp KKKKKKKKKKKKKKKKKKKKKKKKKKK.
